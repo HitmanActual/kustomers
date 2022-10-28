f6ca9c9cbf912a93e02cbe31122d26ac5f0c1e9c
MostafaMahmoud97 <mostafamahmoud111115@gmail.com>
2022-10-28 20:22:34 +0200

    edit paginate in siteServey and utillity bill

--- app/Services/PM/PMService.php
+++ app/Services/PM/PMService.php
@@ -26,7 +26,7 @@ class PMService{
 
         $data = [];
         foreach ($tickets->data as $ticket){
-            return Response::errorResponse($ticket->ticket->opportunity->user->name);
+            return Response::errorResponse($ticket->ticket);
 
 
             $ticket_id = $ticket->ticket->id;
