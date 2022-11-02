a31720dd17fce0d533be6b5d4b2ba3b106b2e816
MostafaMahmoud97 <mostafamahmoud111115@gmail.com>
2022-11-02 20:46:42 +0200

    add finance in Single Ticket

--- app/Services/Ticket/TicketService.php
+++ app/Services/Ticket/TicketService.php
@@ -9,16 +9,11 @@ class TicketService{
     use ConsumesExternalService;
     public $baseUri;
     public $api_key;
-    public function __construct()
-    {
-        $env           = "pm";
-        $this->baseUri = config("gateway_services.$env.base_uri");
-        $this->api_key = config("gateway_services.$env.api_key");
-    }
 
     public function index(){
         $user_id = auth()->user()->lead_id;
         try {
+            $this->initial_api("pm");
             $tickets = json_decode($this->performRequest('get', "tickets/getTickets/".$user_id));
         }catch (\Exception $e){
             return Response::errorResponse('Error Fetch Tickets');
@@ -57,6 +52,7 @@ class TicketService{
 
     public function getTicketById($ticket_id){
         try {
+            $this->initial_api("pm");
             $ticket = json_decode($this->performRequest('get', "tickets/show-single-ticket/".$ticket_id));
         }catch (\Exception $e){
             return Response::errorResponse($e->getMessage());
@@ -64,8 +60,51 @@ class TicketService{
 
         $ticket =  $ticket->data;
 
-        $data = [];
+        //get Finance Status in Single Ticket
 
+        $result = [];
+        $result['opportunity_id'] = $ticket->ticket->opportunity_id;
+        $result['lead_id'] = $ticket->ticket->lead_id;
+        if($ticket->opportunity->is_finance == 1){
+            $result['financial_name'] = $ticket->opportunity->financial_institution;
+            $result['financial'] = "Financed-".$ticket->opportunity->financial_institution;
+            if($ticket->opportunity->financial_institution == "sunlight"){
+
+                try {
+                    $this->initial_api("crm");
+                    $project_ids = json_decode($this->performRequest('get', "v1/sunlight_customer/get-submitted/project_ids/".$result['lead_id']));
+                }catch (\Exception $e){
+                    return Response::errorResponse($e->getMessage());
+                }
+                $result['project_ids'] = $project_ids->data;
+
+            }elseif ($ticket->opportunity->financial_institution == "goodleap"){
+
+                try {
+                    $this->initial_api("crm");
+                    $finance_status = json_decode($this->performRequest('get', "v1/goodleap_customer/get-status/".$result['opportunity_id']));
+                }catch (\Exception $e){
+                    return Response::errorResponse($e->getMessage());
+                }
+
+                $result['finance_status'] = $finance_status;
+            }
+        }else{
+            $result['financial_name'] = "Cache";
+            $result['financial'] = "Cache";
+            $result['finance_status'] = [
+                "loan_amount" => $ticket->contract->cost
+            ];
+        }
+
+
+
+
+        $data = [];
+        $data['finance'] = $result;
+
+
+        //get Docs in Single Ticket
         $data['utility_bill'] = [];
         $data['contract'] = [];
 
@@ -88,12 +127,19 @@ class TicketService{
             return Response::successResponse($data,"Fetch Success");
         }
 
+
         foreach ($plan->data->media as $media){
             array_push($data['plan'],$media->path);
         }
 
 
         return Response::successResponse($data,"Fetch Success");
+    }
+
+    protected function initial_api($type){
+        $env           = $type;
+        $this->baseUri = config("gateway_services.$env.base_uri");
+        $this->api_key = config("gateway_services.$env.api_key");
     }
 
 }
