c8fb4fbf1ce937c83dc010047137aa1a3a98559b
MostafaMahmoud97 <mostafamahmoud111115@gmail.com>
2022-11-04 21:19:32 +0200

    send SiteSurvey and UtilityBill To CRM

--- app/Traits/ConsumesExternalService.php
+++ app/Traits/ConsumesExternalService.php
@@ -26,7 +26,7 @@ trait ConsumesExternalService
         ]);
         $headers['token'] = $this->api_key;
         $headers['Accept'] = 'application/json';
-        $headers['Content-Type'] = 'boundary=-----44cf242ea3173cfa0b97f80c68608c4c';
+        $headers['Content-Type'] = 'multipart/form-data; boundary=-----44cf242ea3173cfa0b97f80c68608c4c';
         $response = $client->request($method, "api/" . $requestUrl, ['multipart' => $formParams, 'headers' => $headers]);
         return $response->getBody()->getContents();
     }
